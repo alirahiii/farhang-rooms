3c33100969dc20e7892bcbc56c8e873a5e56fe35
alirahiii <rahmaniali940@gmail.com>
2026-08-18 13:54:40 +0330

    redirect to prefilled work form from request

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
+from urllib.parse import urlencode
+
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.utils.html import format_html
-from django.utils.text import slugify
 
 from .models import Category, Comment, Work, WorkRequest
 
@@ -31,27 +32,15 @@ class WorkRequestAdmin(admin.ModelAdmin):
 
     @admin.action(description='ساخت اثر از روی این درخواست')
     def create_work_from_request(self, request, queryset):
-        for work_request in queryset:
-            base_slug = slugify(work_request.title) or f'work-{work_request.id}'
-            slug = base_slug
-            suffix = 1
-            while Work.objects.filter(slug=slug).exists():
-                suffix += 1
-                slug = f'{base_slug}-{suffix}'
+        if queryset.count() != 1:
+            self.message_user(request, 'برای این اکشن فقط یک درخواست را انتخاب کنید.', level=messages.ERROR)
+            return
 
-            work = Work.objects.create(
-                title=work_request.title,
-                slug=slug,
-                category=work_request.category,
-                creator=work_request.creator,
-                description=work_request.description,
-                is_published=False,
-            )
-            work_request.status = WorkRequest.STATUS_ADDED
-            work_request.save(update_fields=['status'])
-
-            edit_url = reverse('admin:rooms_work_change', args=[work.id])
-            self.message_user(
-                request,
-                format_html('اثر «{}» ساخته شد. <a href="{}">ویرایش اثر</a>', work.title, edit_url),
-            )
+        work_request = queryset.first()
+        add_url = reverse('admin:rooms_work_add')
+        params = urlencode({
+            'title': work_request.title,
+            'creator': work_request.creator,
+            'category': work_request.category_id,
+        })
+        return HttpResponseRedirect(f'{add_url}?{params}')
