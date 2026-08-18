cf036dd1818aed82b3d41226c34535f84123cebb
alirahiii <rahmaniali940@gmail.com>
2026-08-18 13:48:09 +0330

    add work request admin action and list columns

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.text import slugify
 
 from .models import Category, Comment, Work, WorkRequest
 
@@ -21,4 +24,34 @@ class CommentAdmin(admin.ModelAdmin):
 
 @admin.register(WorkRequest)
 class WorkRequestAdmin(admin.ModelAdmin):
+    list_display = ['title', 'creator', 'category', 'status', 'created_at']
     list_filter = ['status']
+    ordering = ['-created_at']
+    actions = ['create_work_from_request']
+
+    @admin.action(description='ساخت اثر از روی این درخواست')
+    def create_work_from_request(self, request, queryset):
+        for work_request in queryset:
+            base_slug = slugify(work_request.title) or f'work-{work_request.id}'
+            slug = base_slug
+            suffix = 1
+            while Work.objects.filter(slug=slug).exists():
+                suffix += 1
+                slug = f'{base_slug}-{suffix}'
+
+            work = Work.objects.create(
+                title=work_request.title,
+                slug=slug,
+                category=work_request.category,
+                creator=work_request.creator,
+                description=work_request.description,
+                is_published=False,
+            )
+            work_request.status = WorkRequest.STATUS_ADDED
+            work_request.save(update_fields=['status'])
+
+            edit_url = reverse('admin:rooms_work_change', args=[work.id])
+            self.message_user(
+                request,
+                format_html('اثر «{}» ساخته شد. <a href="{}">ویرایش اثر</a>', work.title, edit_url),
+            )
