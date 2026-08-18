b87fbbe593bfc2047ab25920cd2783adf107ce96
alirahiii <rahmaniali940@gmail.com>
2026-08-18 11:46:26 +0330

    add base template with RTL and Tailwind

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("سایت کار می‌کند")
+    return render(request, 'rooms/home.html')
