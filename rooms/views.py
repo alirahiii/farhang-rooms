4a9f9df74bcd0a44e1ea016b28288dd82d79e522
alirahiii <rahmaniali940@gmail.com>
2026-08-18 11:51:10 +0330

    add homepage with categories and works

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Category, Work
+
 
 def home(request):
-    return render(request, 'rooms/home.html')
+    categories = Category.objects.all()
+    latest_works = Work.objects.filter(is_published=True).select_related('category').order_by('-created_at')[:8]
+    return render(request, 'rooms/home.html', {
+        'categories': categories,
+        'latest_works': latest_works,
+    })
